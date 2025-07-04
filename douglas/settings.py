33ef2bb8dfb0eb7172204a49c94805cf37d99569
Douglas Ribeiro da Rosa <douglasribeirodarosa@gmail.com>
2025-07-04 12:48:24 -0300

    index

--- douglas/settings.py
+++ douglas/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'douglas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/public')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
