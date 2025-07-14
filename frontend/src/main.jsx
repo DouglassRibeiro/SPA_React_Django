2d7dc5540cdba043d1fd80b664b8ff5926c8d1ac
Douglas Ribeiro da Rosa <douglasribeirodarosa@gmail.com>
2025-07-13 21:05:15 -0300

    reorganizing - pascalcase

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './Index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// AQUI ESTÁ O CONTROLE!
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
