26ceecac2ff0a72f10f77517e31d20fe55577da4
Douglas Ribeiro da Rosa <douglasribeirodarosa@gmail.com>
2025-08-02 23:19:33 -0300

    add experience icons in home | all .svg

---
+++ frontend/src/assets/tech-icons/index.js
@@ -0,0 +1,43 @@
+import icon3dBox from './icon3dBox.svg';
+import icon3dRotation from './icon3dRotation.svg';
+import iconAngular from './iconAngular.svg';
+import iconAzureSqlDataBase from './iconAzureSqlDataBase.svg';
+import iconBlenderIcon from './iconBlenderIcon.svg';
+import iconCMoreMoreTraced from './iconCMoreMoreTraced.svg';
+import iconDataBaseGlobalityVelocity from './iconDataBaseGlobalityVelocity.svg';
+import iconDjango from './iconDjango.svg';
+import iconGit from './iconGit.svg';
+import iconGolang from './iconGolang.svg';
+import iconHtml from './iconHtml.svg';
+import iconHtmliconHtmlEnd from './iconHtmlEnd.svg';
+import iconHtmlStart from './iconHtmlStart.svg';
+import iconJava from './iconJava.svg';
+import iconJavaScript from './iconJavaScript.svg';
+import iconNode from './iconNode.svg';
+import iconPython from './iconPython.svg';
+import iconReact from './iconReact.svg';
+import iconTailwind from './iconTailwind.svg';
+import iconVue from './iconVue.svg';
+
+export {
+    icon3dBox,
+    icon3dRotation,
+    iconAngular,
+    iconAzureSqlDataBase,
+    iconBlenderIcon,
+    iconCMoreMoreTraced,
+    iconDataBaseGlobalityVelocity,
+    iconDjango,
+    iconGit,
+    iconGolang,
+    iconHtml,
+    iconHtmlStart,
+    iconHtmliconHtmlEnd,
+    iconJava,
+    iconJavaScript,
+    iconNode,
+    iconPython,
+    iconReact,
+    iconTailwind,
+    iconVue,
+}
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,9 +1,32 @@
 // src/pages/Home.jsx
 import React from "react";
 import styles from "./Home.module.css"; // 1. Importe o arquivo de estilos
-import profileImage from "../../assets/profile-photo.png";
+import profileImage from "../../assets/profile-photo.svg";
 import ReflectivePhrase from "../../components/ReflectivePhrase";
 import homeIcon from "../../assets/icons/home.svg";
+import * as techIconsList from '../../assets/tech-icons/';
+
+const techIcons = [
+  {id: '3dBox', src: techIconsList.icon3dBox, alt: '3dBox'},
+  {id: 'icon3dRotation', src: techIconsList.icon3dRotation, alt: 'icon3dRotation'},
+  {id: 'iconAngular', src: techIconsList.iconAngular, alt: 'iconAngular'},
+  {id: 'iconAzureSqlDataBase', src: techIconsList.iconAzureSqlDataBase, alt: 'iconAzureSqlDataBase'},
+  {id: 'iconBlenderIcon', src: techIconsList.iconBlenderIcon, alt: 'iconBlenderIcon'},
+  {id: 'iconCMoreMoreTraced', src: techIconsList.iconCMoreMoreTraced, alt: 'iconCMoreMoreTraced'},
+  {id: 'iconDjango', src: techIconsList.iconDjango, alt: 'iconDjango'},
+  {id: 'iconGit', src: techIconsList.iconGit, alt: 'iconGit'},
+  {id: 'iconGolang', src: techIconsList.iconGolang, alt: 'iconGolang'},
+  {id: 'iconHtml', src: techIconsList.iconHtml, alt: 'iconHtml'},
+  {id: 'iconHtmlStart', src: techIconsList.iconHtmlStart, alt: 'iconHtmlStart'},
+  {id: 'iconHtmliconHtmlEnd', src: techIconsList.iconHtmliconHtmlEnd, alt: 'iconHtmliconHtmlEnd'},
+  {id: 'iconJava', src: techIconsList.iconJava, alt: 'iconJava'},
+  {id: 'iconJavaScript', src: techIconsList.iconJavaScript, alt: 'iconJavaScript'},
+  {id: 'iconNode', src: techIconsList.iconNode, alt: 'iconNode'},
+  {id: 'iconPython', src: techIconsList.iconPython, alt: 'iconPython'},
+  {id: 'iconReact', src: techIconsList.iconReact, alt: 'iconPython'},
+  {id: 'iconTailwind', src: techIconsList.iconTailwind, alt: 'iconTailwind'},
+  {id: 'iconVue', src: techIconsList.iconVue, alt: 'iconVue'},
+]
 
 function Home() {
   return (
@@ -18,7 +41,9 @@ function Home() {
           <p className={styles.subtitle}>
             Sou um desenvolvedor sempre em busca de novas tecnologias.
           </p>
-          <div className={styles.teste} ></div>
+          <div className={styles.iconMarques}>
+            {techIcons.map(icon => (<img key = {icon.id} src={icon.src} alt = {icon.alt} className={styles.techIcon}/>))}
+          </div>
         </div>
         <div className={styles.imageContainer}>
           <img
@@ -28,7 +53,9 @@ function Home() {
           />
         </div>
       </div>
-      <ReflectivePhrase />
+      <div>
+        <ReflectivePhrase/>
+      </div>
     </>
   );
 }
